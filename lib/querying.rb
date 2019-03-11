3ff138208d8b0443213342c1882e994471926b7f
davisjustinw <davisjustinw@gmail.com>
2019-03-10 20:32:13 -0700

    selects the series title with the most characters that are the species human was false positive I think

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
     JOIN characters ON characters.series_id = series.id
     WHERE characters.species = 'human'
     GROUP BY series.id
-    ORDER BY series.id ASC
+    ORDER BY COUNT(series.id) DESC 
     LIMIT 1"
 end
 
